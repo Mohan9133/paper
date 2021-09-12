17630f65ece66f8e7aa46153c598f14711977ec0
jaganmohan <jaganmoha50@gmail.com>
2021-09-12 11:17:12 +0530

    dark places have room for light

---
+++ mohan/src/main/java/1432/trinadh.java
@@ -0,0 +1,9 @@
+package 1432;
+
+public class \trinadh 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ mohan/src/test/java/1432/trinadhTest.java
@@ -0,0 +1,38 @@
+package 1432;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class \trinadhTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public \trinadhTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( \trinadhTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void test\trinadh()
+    {
+        assertTrue( true );
+    }
+}
